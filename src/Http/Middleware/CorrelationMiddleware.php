e00bb2ff2fc0c714d5066e15f67aab22b3dd1595
Mohamed Ahmed <mohamedabdelmenem01@gmail.com>
2025-02-12 02:35:02 +0200

    update header

--- src/Http/Middleware/CorrelationMiddleware.php
+++ src/Http/Middleware/CorrelationMiddleware.php
@@ -13,11 +13,7 @@ class CorrelationMiddleware
     {
         $headerName = config('correlation.header', 'X-Correlation-ID');
         $correlationId = $request->header($headerName) ?? Str::uuid()->toString();
-        if (method_exists(\Log::class, 'shareContext')) {
-            \Log::shareContext(['correlation_id' => $correlationId]);
-        }else{
-            config(['logging.context.correlation_id' => $correlationId]);
-        }
+
         app()->instance('correlation.id', $correlationId);
         
         $response = $next($request);
